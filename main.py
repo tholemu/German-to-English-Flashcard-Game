f2f880d37af5bf5ee9ac2459f0be292c871c2faf
bowler <m.bowler@f5.com>
2023-03-23 23:31:24 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
 from tkinter import *
 import pandas
+import json
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
-to_learn = {}
+words_json = {}
 
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = open("data/words_to_learn.json")
 except FileNotFoundError:
-    #original_data = pandas.read_csv("data/french_words.csv")
-    original_data = pandas.read_csv("data/german_words.csv")
-    to_learn = original_data.to_dict(orient="records")
+    file = open("data/basic_words_in_german.json")
+    words_json = json.load(file)
 else:
-    to_learn = data.to_dict(orient="records")
+    words_json = json.load(data)
 
 
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(to_learn)
-    #canvas.itemconfig(card_title, text="French", fill="black")
-    #canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    current_card = random.choice(words_json)
     canvas.itemconfig(card_title, text="German", fill="black")
     canvas.itemconfig(card_word, text=current_card["German"], fill="black")
-    canvas.itemconfig(card_background, image=card_front_img)
+    canvas.itemconfig(card_background, image="")
     flip_timer = window.after(5000, func=flip_card)
 
 
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
-    canvas.itemconfig(card_background, image=card_back_img)
+    canvas.itemconfig(card_background, image="")
 
 
 def is_known():
-    to_learn.remove(current_card)
-    print(len(to_learn))
-    data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    words_json.remove(current_card)
+    file_to_learn = open("data/words_to_learn.json", "w+")
+    file_to_learn.write(json.dumps(words_json))
     next_card()
 
 
@@ -49,20 +46,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(5000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400, 263, image=card_front_img)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image="")
 card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 264, text="word", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-wrong_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
+# wrong_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image="", highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
-right_image = PhotoImage(file="images/right.png")
-known_button = Button(image=right_image, highlightthickness=0, command=is_known)
+# right_image = PhotoImage(file="images/right.png")
+known_button = Button(image="", highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
